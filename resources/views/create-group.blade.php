9e46ca73c0935f7cc88b77a36651df431ab444c2
nanan4th <natvicdreh@gmail.com>
2021-11-30 22:00:37 +0700

    view add form

--- resources/views/create-group.blade.php
+++ resources/views/create-group.blade.php
@@ -1,27 +1,56 @@
 @extends('layouts.app')
 
 @section('content')
-    <h1>Add Group</h1>
+<section id="forms">
+    <div class="container-lg">
+        <h2>Add Group</h2>
+        <p class="lead">Start your path with making a group</p>
+    </div>
+
     @if ($errors->any())
-        <div class="alert alert-danger">
-            <ul>
-                @foreach ($errors->all() as $error)
-                    <li>{{ $error }}</li>
-                @endforeach
-            </ul>
-        </div>
+    <div class="alert alert-danger">
+        <ul>
+            @foreach ($errors->all() as $error)
+            <li>{{ $error }}</li>
+            @endforeach
+        </ul>
+    </div>
     @endif
+    <div class="row justify-content-center my-6">
+
+    </div>
+
     <form action="/group/create" method="POST">
         @csrf
-        <label for="">Group name</label>
-        <input type="text" name="name"><br>
-        <label for="">Group Type</label><br>
-        <input type="radio" name="type" id="" value="Spotify">
-        <label for="">Spotify</label><br>
-        <input type="radio" name="type" id="" value="Netflix">
-        <label for="">Netflix</label><br>
-        <label for="">Group Desc</label><br>
-        <textarea name="desc" id="" cols="30" rows="10"></textarea><br>
-        <button type="submit">Add group</button>
+        <div class="row mb-3">
+            <label for="" class="col-sm-2 col-form-label">Group name</label>
+            <div class="col-sm-10">
+                <input type="text" class="form-control" name="name">
+            </div>
+        </div>
+        <fieldset class="row mb-3">
+            <legend class="col-form-label col-sm-2 pt-0">Group Type</legend>
+            <div class="col-sm-10">
+                <div class="form-check">
+                    <input class="form-check-input" type="radio" name="type" id="gridRadios1" value="Spotify" checked>
+                    <label class="form-check-label" for="gridRadios1">
+                        Spotify
+                    </label>
+                </div>
+                <div class="form-check">
+                    <input class="form-check-input" type="radio" name="type" id="gridRadios2" value="Netflix">
+                    <label class="form-check-label" for="gridRadios2">
+                        Netflix
+                    </label>
+                </div>
+            </div>
+        </fieldset>
+        <div class="form-floating">
+            <label for="" class="col-sm-2 col-form-label">Group Desciption</label>
+            <textarea class="form-control" name="desc" id="" placeholder="Leave a description here"></textarea>
+        </div>
+        <br>
+        <button type="submit" class="btn btn-primary">Add group</button>
     </form>
+</section>
 @endsection
